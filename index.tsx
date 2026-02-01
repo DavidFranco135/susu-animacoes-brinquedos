474f3e125ec93af2fed392c0b48869adef34bfc7
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-01 02:20:57 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // 👈 ISSO É OBRIGATÓRIO (Tailwind)
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
