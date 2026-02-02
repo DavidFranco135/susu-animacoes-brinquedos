f9d45e10124b036df7d4f7dbbab6fe0b981b0081
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-02 17:17:09 -0300

    Create firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUvwY-e7h0KZyFJv7n0ignpzlMUGJIurU",
+  authDomain: "niklaus-b2b.firebaseapp.com",
+  projectId: "niklaus-b2b",
+  storageBucket: "niklaus-b2b.firebasestorage.app",
+  messagingSenderId: "936430517671",
+  appId: "1:936430517671:web:6a0f1b86a39621d74c4a82",
+  measurementId: "G-3VGKJGWFSY"
+};
+
+// Inicializa o Firebase apenas se ainda não foi inicializado
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
+// Exporta as instâncias
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
